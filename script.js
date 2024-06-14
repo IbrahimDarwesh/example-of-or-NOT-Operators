42058505c54ced6d51a96b36c9b62107cbb0992c
IbrahimDarwesh <167519494+IbrahimDarwesh@users.noreply.github.com>
2024-06-14 11:19:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//let age=prompt("Enter Your Age")
+//if (age>=18 && age<=59){
+ //   alert("Your Eligibale");}
+//else if (age<18 || age>=60){
+  //  alert("Your Not Eligibale");
+//}
+// let Login=prompt("ENTER YOUR LOGIN NAME")
+// let Password=prompt("ENTER YOUR PASSWORD")
+// if (Login == "admin" || Password == "admin" ){
+//     alert("Login SuccessFul");
+// }
+// else{ alert("Invilde Login or Password")}
+// let a = prompt ("Enter digits From 1 to 24 To show You its day time or Night")
+// if (a >= 7 && a <=19){
+//     alert("Its Day Time");
+// } 
+// else { alert("Its Night Time")
+// }
+let a = prompt("Enter the Number")
+if (a %2 !=0){
+    alert("its Odd Number")
+}
+else { alert("its Even Number")}
+
